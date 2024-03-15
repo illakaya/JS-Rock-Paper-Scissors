e00a9ff628d5867720391dc9381b0d48c7d21f6e
illakaya <mimijane.nguyen@gmail.com>
2024-03-15 17:32:30 +1100

    created functions to alert the user of the game stats

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 // Create a variable to hold valid user input in the global scope as it will be accessed by multiple functions
 let userHand = ``;
 
+// Create a variable to hold pc input in the global scope as it will be accessed by multiple functions
+let pcHand = ``;
+
+// Create an object that will store the user's data
+let gameData = {
+    gamesPlayed: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+    moveHistory: {
+        rock: 0,
+        paper: 0,
+        scissors: 0,
+    }
+};
+
+// Create a function that will count what hand the user has chosen so far and store it in the object gameData
+function moveCount(hand) {
+    switch (hand) {
+        case `R`:
+            ++gameData.moveHistory.rock;
+            break;
+        case `P`:
+            ++gameData.moveHistory.paper;
+            break;
+        case `S`:
+            ++gameData.moveHistory.scissors;
+            break;
+    };
+}
+
 // Create a function that will loop until the user enters a correct input
 function validInput(hand) {
     // Start off by creating a prompt in which users can enter a value and store it in a variable
@@ -15,6 +46,7 @@ function validInput(hand) {
             // if it is a valid input, store input into userHand
             if (rps.includes(hand)) {
                 userHand = hand;
+                moveCount(hand);
                 console.log(`User choice: ${hand}`);
                 // if it is not a valid input, ask user to try again
             } else {
@@ -24,9 +56,6 @@ function validInput(hand) {
             }
         };
 }
-
-// Create a variable to hold pc input in the global scope as it will be accessed by multiple functions
-let pcHand = ``;
 
 // Create a function that will random generate RPS for the computer
 function generateHand() {
@@ -44,26 +73,14 @@ function generateHand() {
         case 2:
             pcHand = `P`;
             break;
-    }
+    };
     console.log(`PC choice: ${pcHand}`);
 }
 
-// Create an object that will store the user's data
-let gameData = {
-    gamesPlayed: 0,
-    wins: 0,
-    losses: 0,
-    draws: 0,
-    moveHistory: {
-        rock: 0,
-        paper: 0,
-        scissors: 0,
-    }
-}
-
+// Create a function that decides if the user has won, lost or drew
 function gameResult(user, pc) {
     if (user === pc) {
-        return 'tie';
+        return 'draw';
     }
     if (user === `R` && pc === `S`) {
         return `win`;
@@ -85,6 +102,45 @@ function gameResult(user, pc) {
     }
 }
 
+// create a var to store the results of each game
+let wld = ``;
+
+// Create a function that will count the results of each game and store it in the object gameData
+function resultCount(result) {
+    ++gameData.gamesPlayed;
+    switch (result) {
+        case `draw`:
+            ++gameData.draws;
+            break;
+        case `win`:
+            ++gameData.wins;
+            break;
+        case `lose`:
+            ++gameData.losses;
+            break;
+    };
+    console.log(wld);
+}
+
+// Create function that will alert the user of: pc hand, result of the game
+function choiceResult() {
+    alert(`PC has chosen ${pcHand}.`);
+    alert(`You ${wld}!`)
+}
+
+// Create a function that will alert the user of the stats
+function stats() {
+    alert(`
+    Games played: ${gameData.gamesPlayed}
+        Wins: ${gameData.wins}
+        Draws: ${gameData.draws}
+        Loses: ${gameData.losses}
+    Move History
+        Rock: ${gameData.moveHistory.rock}
+        Paper: ${gameData.moveHistory.paper}
+        Scissors: ${gameData.moveHistory.scissors}`);
+}
+
 let gameContinue = true;
 
 // create a loop that will end when the user cancels
@@ -93,13 +149,13 @@ while (gameContinue) {
     validInput();
     // Call generateHand function
     generateHand();
-    console.log(gameResult(userHand, pcHand));
-    gameData.gamesPlayed = gameData.gamesPlayed + 1;
+    wld = gameResult(userHand, pcHand);
+    resultCount(wld);
+    choiceResult();
     let userStillGame = confirm(`Do you still want to continue?`);
     if (userStillGame === false) {
         gameContinue = false;
     }
 };
 
-console.log(`Games played: ${gameData.gamesPlayed}`);
-console.log(gameData);
+stats();
